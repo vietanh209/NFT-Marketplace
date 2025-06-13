da98cc98d4aa5435b2b3a60ee0c8c92b6f75ac10
tysanh1 <168287275+tysanh1@users.noreply.github.com>
2025-06-13 20:07:21 +0700

    up

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -57,8 +57,8 @@ const HeroSection = () => {
                                     <div className="flex gap-5">
                                     <Avatar isBordered radius="full" size="md" src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
                                     <div className="flex flex-col gap-1 items-start justify-center">
-                                        <h4 className="text-small font-semibold leading-none text-default-600">Kiem Pham</h4>
-                                        <h5 className="text-small tracking-tight text-default-400">@Kiem Pham</h5>
+                                        <h4 className="text-small font-semibold leading-none text-default-600">Việt Anh</h4>
+                                        <h5 className="text-small tracking-tight text-default-400">@tys.209</h5>
                                     </div>
                                     </div>
                                     <Button
@@ -81,9 +81,9 @@ const HeroSection = () => {
                                         className="text-blue-500 hover:text-blue-600"
                                         isExternal
                                         showAnchorIcon
-                                        href="https://www.facebook.com/m3iK18dp.dev/"
+                                        href="https://www.facebook.com/tys.209"
                                     >
-                                        #DevWithKiem 
+                                        #vietanh209
                                     </Link>
                                     <span className="py-2" aria-label="computer" role="img">
                                         💻
@@ -92,7 +92,7 @@ const HeroSection = () => {
                                 </CardBody>
                                 <CardFooter className="gap-3">
                                     <div className="flex gap-1">
-                                    <p className="font-semibold text-default-400 text-small">4</p>
+                                    <p className="font-semibold text-default-400 text-small">4K</p>
                                     <p className=" text-default-400 text-small">Following</p>
                                     </div>
                                     <div className="flex gap-1">
